2867656ad5c5abf80c6a2492fa995e050c93e98a
Javian <52677265+JavianTheGreek@users.noreply.github.com>
2021-02-17 14:10:42 -0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,14 +5,17 @@ from discord.ext import commands
 discordPass = ''
 
 # Commands prefic inwhich the bot responses to..
-client = commands.Bot(command_prefix='x')
+client = commands.Bot(command_prefix='!!')
 
 @client.event
 async def on_ready():
     print('The Bot is online has we speak....')
+
+
 @client.event
 async def on_message(msg):
-    if msg.content.find('xhello') != -1:
+    if msg.content.find('Hello') != -1:
         await msg.channel.send('Wassup G')
+        
 
 client.run(discordPass)
